0b01ef1f9f5d99f64431dbbd3ca99421beac9d7a
yejin <yejin5480@naver.com>
2025-04-05 22:02:30 +0900

    step1 이력서선택

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import LoginPage from './pages/LoginPage';
 import SignupPage from './pages/SignupPage';
 import AdminDashboard from './adminPages/AdminDashboard';
 import Step1 from './pages/Step1Page';
-import Step01 from './pages/Step1';
 import Step02 from './pages/Step2';
 import Step03 from './pages/Step3';
 import Step04 from './pages/Step4';
@@ -28,7 +27,6 @@ function App() {
         <Route path="/signup" element={<SignupPage language={language} onChangeLanguage={setLanguage} />} />
         <Route path="/admin" element={<AdminDashboard language={language} onChangeLanguage={setLanguage} />} />
         <Route path="/step1" element={<Step1 language={language} onChangeLanguage={setLanguage} />} />
-        <Route path="/step01" element={<Step01 language={language} onChangeLanguage={setLanguage} />} />
         <Route path="/step02" element={<Step02 language={language} onChangeLanguage={setLanguage} />} />
         <Route path="/step03" element={<Step03 language={language} onChangeLanguage={setLanguage} />} />
         <Route path="/step04" element={<Step04 language={language} onChangeLanguage={setLanguage} />} />
